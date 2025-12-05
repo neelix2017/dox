771e17808bf76ef1e2597ce511b5330b92ad2f76
neelix2017 <33653826+neelix2017@users.noreply.github.com>
2025-12-05 21:51:53 +0100

    Refactor keydown event handling in main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 document.addEventListener("keydown", (e) => {
 const frame = document.getElementById("myFrame").contentWindow;
-alert(e.keyCode)
+//alert(e.keyCode)
 
 // Translate arrow keys to scroll events
-if (e.key === "ArrowUp" || e.keyCode === 38) frame.scrollBy(0, -50);
-if (e.key === "ArrowDown" || e.keyCode === 40) frame.scrollBy(0, 50);
-if (e.key === "ArrowLeft" || e.keyCode === 37) frame.scrollBy(-50, 0);
-if (e.key === "ArrowRight" || e.keyCode === 39) frame.scrollBy(50, 0);
-
+if ( e.keyCode === 38) frame.scrollBy(0, -50);  //"ArrowUp"
+if ( e.keyCode === 40) frame.scrollBy(0, 50);  //"ArrowDown"
+if ( e.keyCode === 37) frame.scrollBy(-50, 0);  //"ArrowLeft"
+if ( e.keyCode === 39) frame.scrollBy(50, 0);  //"ArrowRight
+if ( e.keyCode === 10009) alert('Back');  //"Back"
+if ( e.keyCode === 10252) alert('Play');  //"Play"
 
 // Enter key → click center
-if (e.key === "Enter" || e.keyCode === 13) {
+if (  e.keyCode === 13) {  //"Enter"
 const centerX = frame.innerWidth / 2;
 const centerY = frame.innerHeight / 2;
 const evt = new frame.MouseEvent("click", {
@@ -21,6 +22,7 @@ bubbles: true
 frame.dispatchEvent(evt);
 }
 });
+
 
 
 
