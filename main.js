d290194a6c07261ce281bfd8fdf13372c51b591a
neelix2017 <33653826+neelix2017@users.noreply.github.com>
2025-12-05 21:41:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const frame = document.getElementById("myFrame").contentWindow;
 
 
 // Translate arrow keys to scroll events
-if (e.key === "ArrowUp") frame.scrollBy(0, -50);
-if (e.key === "ArrowDown") frame.scrollBy(0, 50);
-if (e.key === "ArrowLeft") frame.scrollBy(-50, 0);
-if (e.key === "ArrowRight") frame.scrollBy(50, 0);
+if (e.key === "ArrowUp" || e.keyCode === 38) frame.scrollBy(0, -50);
+if (e.key === "ArrowDown" || e.keyCode === 40) frame.scrollBy(0, 50);
+if (e.key === "ArrowLeft" || e.keyCode === 37) frame.scrollBy(-50, 0);
+if (e.key === "ArrowRight" || e.keyCode === 39) frame.scrollBy(50, 0);
 
 
 // Enter key → click center
-if (e.key === "Enter") {
+if (e.key === "Enter" || e.keyCode === 13) {
 const centerX = frame.innerWidth / 2;
 const centerY = frame.innerHeight / 2;
 const evt = new frame.MouseEvent("click", {
@@ -21,4 +21,5 @@ bubbles: true
 frame.dispatchEvent(evt);
 }
 });
+
 
